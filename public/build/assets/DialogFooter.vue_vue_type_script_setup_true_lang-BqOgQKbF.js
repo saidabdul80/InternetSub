b51f71d442e7d670b1177d040afd4e2e98f1c59a
Said Abdulsalam <sabdulsalam@ubtconsults.se>
2026-01-21 15:50:45 +0100

    fix

---
+++ public/build/assets/DialogFooter.vue_vue_type_script_setup_true_lang-BqOgQKbF.js
@@ -0,0 +1,1 @@
+import{c as a}from"./index-Dv5PaFr3.js";import{d as r,a as t,D as l,u as n,A as c,o as f}from"./app-Bbpl4-Pg.js";const d=r({__name:"DialogFooter",props:{class:{}},setup(s){const e=s;return(o,m)=>(f(),t("div",{"data-slot":"dialog-footer",class:l(n(a)("flex flex-col-reverse gap-2 sm:flex-row sm:justify-end",e.class))},[c(o.$slots,"default")],2))}});export{d as _};
